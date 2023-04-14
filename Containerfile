6eb0e424a931a1c45dff6333f8ce1a768475a491
Jonatan Steuernagel <jonatan@jsteuernagel.de>
2023-04-14 07:50:12 +0200

    Add initial Containerfile

---
+++ Containerfile
@@ -0,0 +1,19 @@
+ARG IMAGE_NAME="${IMAGE_NAME:-silverblue}"
+ARG SOURCE_IMAGE="${SOURCE_IMAGE:-silverblue}"
+ARG BASE_IMAGE="quay.io/fedora-ostree-desktops/${SOURCE_IMAGE}"
+ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-37}"
+
+FROM ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION} AS builder
+
+ARG IMAGE_NAME="${IMAGE_NAME}"
+ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION}"
+
+COPY build.sh /tmp/build.sh
+COPY post-install.sh /tmp/post-install.sh
+COPY packages.txt /tmp/packages.txt
+
+RUN /tmp/build.sh
+RUN /tmp/post-install.sh
+RUN rm -rf /tmp/* /var/*
+RUN ostree container commit
+RUN mkdir -p /var/tmp && chmod -R 1777 /var/tmp
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# shellcheck disable=SC2086,SC2048
+trim_all() {
+    # Usage: trim_all "   example   string    "
+
+    # Disable globbing to make the word-splitting below safe.
+    set -f
+
+    # Set the argument list to the word-splitted string.
+    # This removes all leading/trailing white-space and reduces
+    # all instances of multiple spaces to a single ("  " -> " ").
+    set -- $*
+
+    # Print the argument list as a string.
+    printf '%s\n' "$*"
+
+    # Re-enable globbing.
+    set +f
+}
+
+[ -e "/tmp/packages" ] \
+  && packages=$(cat /tmp/packages.txt) \
+  || packages=$(cat packages.txt)
+
+trimmed_packages=$(trim_all "$packages")
+
+rpm-ostree install $trimmed_packages
