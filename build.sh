6b1e1f0670672d40310e8049070fb105103841de
Jonatan Steuernagel <jonatan@jsteuernagel.de>
2023-04-20 18:26:53 +0200

    build.sh: Remove initramfs part

--- build.sh
+++ build.sh
@@ -37,7 +37,4 @@ rpm --import https://downloads.1password.com/linux/keys/1password.asc
 echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
 rpm-ostree install 1password
 
-# Enable initramfs generation to fix keymap in LUKS
-#rpm-ostree initramfs --enable
-
 pip3 install --prefix=/usr yafti
