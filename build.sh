3b556912c14f46e74754f8654188ec5027067b38
Jonatan Steuernagel <jonatan@jsteuernagel.de>
2023-04-22 15:27:52 +0200

    Revert "Change 1Password install method"
    This reverts commit de7740ac72952034fdba697ab5fa05c784483fde.

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ RELEASE="$(rpm -E %fedora)"
 
 wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
-    https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
 rpm-ostree install \
     /tmp/rpms/*.rpm \
@@ -32,5 +31,10 @@ do
 done < "/tmp/install-packages.txt"
 
 rpm-ostree install ${installPackages}
+
+# 1Password https://support.1password.com/install-linux/#centos-fedora-or-red-hat-enterprise-linux
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://downloads.1password.com/linux/keys/1password.asc" > /etc/yum.repos.d/1password.repo
+rpm-ostree install 1password
 
 pip3 install --prefix=/usr yafti
